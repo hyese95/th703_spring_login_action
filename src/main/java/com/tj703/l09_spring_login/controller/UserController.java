328126572e8dcea39aa889e9450b4d169ea9d061
hyese95 <hyese3085@gmail.com>
2025-04-16 09:50:37 +0900

    jwt

--- src/main/java/com/tj703/l09_spring_login/controller/UserController.java
+++ src/main/java/com/tj703/l09_spring_login/controller/UserController.java
@@ -1,10 +1,11 @@
 package com.tj703.l09_spring_login.controller;
 
 import com.tj703.l09_spring_login.entity.User;
-import com.tj703.l09_spring_login.security.CustomUserDetails;
+import com.tj703.l09_spring_login.jwt.JwtUtil;
 import com.tj703.l09_spring_login.security.CustomUserDetailsService;
 import com.tj703.l09_spring_login.service.UserService;
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,9 +16,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Objects;
-import java.util.Optional;
-
 @Controller
 @RequestMapping("/user")
 @AllArgsConstructor
@@ -25,6 +23,7 @@ public class UserController {
 
     private final UserService userService;
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @GetMapping("/login.do")
@@ -41,7 +40,8 @@ public class UserController {
     // ?id=admin1&pw=1234
     @PostMapping("/jwt/login.do")
     public String loginAction(
-            @ModelAttribute User user
+            @ModelAttribute User user,
+            HttpServletResponse response
             ) {
         System.out.println("loginAction 중");
         UserDetails userDetails = null;
@@ -54,6 +54,12 @@ public class UserController {
         ); // 로그인 -> 시큐리티에서 관리하는 유저 객체 토큰 발급
         SecurityContextHolder.getContext().setAuthentication(authToken);
         // 시큐리티에서 관리하는 로그인 유저 (UserDetails)
+        Cookie jwt=new Cookie("jwt", jwtUtil.generateToken(userDetails.getUsername()));
+        jwt.setHttpOnly(true); // JS 에서 쿠키탈취 못하게
+        // jwt.setSecure(true); // https 통신에서만 쿠키보내겠다.
+        jwt.setPath("/");
+        jwt.setMaxAge(1000*60*30);
+        response.addCookie(jwt);
         return "redirect:/";
     }
     @GetMapping("/logout.do")
